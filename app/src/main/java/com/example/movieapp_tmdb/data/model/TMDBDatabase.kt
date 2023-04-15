3e2300438a94a605cdf5d4c13684cb11552dd53c
yashkys <yashkhaitan02@gmail.com>
2023-04-16 01:37:49 +0530

    added TMDBDatabase class

---
+++ app/src/main/java/com/example/movieapp_tmdb/data/model/TMDBDatabase.kt
@@ -0,0 +1,21 @@
+package com.example.movieapp_tmdb.data.model
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.movieapp_tmdb.data.ArtistDao
+import com.example.movieapp_tmdb.data.MovieDao
+import com.example.movieapp_tmdb.data.TvShowDao
+import com.example.movieapp_tmdb.data.model.artist.Artist
+import com.example.movieapp_tmdb.data.model.movie.Movie
+import com.example.movieapp_tmdb.data.model.tvShow.TVShow
+
+@Database(
+    entities = [Movie::class, TVShow::class, Artist::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class TMDBDatabase: RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+    abstract fun tvDao(): TvShowDao
+    abstract fun artistDao(): ArtistDao
+}
