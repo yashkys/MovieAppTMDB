43872ecbcf5b936eb8146ab11c5bae9756050ebd
yashkys <yashkhaitan02@gmail.com>
2023-04-16 15:00:14 +0530

    modified TvShowActivity and activity_tv_show.xml, and also add TvShowAdapter

--- app/src/main/java/com/example/movieapp_tmdb/presentation/di/Injector.kt
+++ app/src/main/java/com/example/movieapp_tmdb/presentation/di/Injector.kt
@@ -6,6 +6,6 @@ import com.example.movieapp_tmdb.presentation.di.tvshow.TvShowSubComponent
 
 interface Injector {
     fun createMovieSubComponent(): MovieSubComponent
-    fun creteTvShowSubComponent(): TvShowSubComponent
-    fun creteArtistSubComponent(): ArtistSubComponent
+    fun createTvShowSubComponent(): TvShowSubComponent
+    fun createArtistSubComponent(): ArtistSubComponent
 }
